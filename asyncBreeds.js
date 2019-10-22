958ed0bb4c1f4556fbbea863c0d4cb5c44710773
Amy Bemister <amy.bemister@gmail.com>
2019-10-22 23:29:20 +0000

    Mocha and chai based tests for async funcs

---
+++ asyncBreeds.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+
+const breedDetailsFromFile = function(breed, callback) {
+  console.log('breedDetailsFromFile: Calling readFile...');
+  fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
+    // Pass data back via callback instead of return
+    console.log('Callback: I have the data!')
+    if (!error) {
+      callback(data);
+    } else {
+      callback(undefined);
+    }
+  });
+}
+
+module.exports = breedDetailsFromFile;
